54d34c8d1f6fcb97892002b64eb8ebb79f4a0684
chell2 <rei1259@gmail.com>
2025-01-04 12:24:56 +0900

    未使用変数の削除

--- app/api/beers/route.ts
+++ app/api/beers/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { Database } from '@/types/supabase';
