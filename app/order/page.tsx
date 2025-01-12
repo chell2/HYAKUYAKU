79bf10d3c12ea2ecebac03b07751efc60acf7313
chell2 <rei1259@gmail.com>
2025-01-12 19:57:48 +0900

    認証修正テスト

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,30 +1,31 @@
 import { redirect } from 'next/navigation';
 import { createClient } from '@/lib/utils/supabase/server';
 
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
 import OrderCard from '@/components/OrderCard';
-import { Database } from '@/types/supabase';
+// import { Database } from '@/types/supabase';
 
-const supabase = createServerComponentClient<Database>({ cookies });
-
-const getAllOrder = async () => {
-  const { data: orderlist } = await supabase
-    .from('orders')
-    .select(
-      '*, client:clients(*), order_items(created_at, id, order_id, product_id, quantity, product:products(*, brewery:breweries(*)))'
-    )
-    .order('order_date', { ascending: false });
-  return orderlist;
-};
-
+// const supabase = createServerComponentClient<Database>({ cookies });
 export default async function OrderList() {
-  const orderlist = await getAllOrder();
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
   if (error || !data?.user) {
     redirect('/login');
   }
+
+  const getAllOrder = async () => {
+    const { data: orderlist } = await supabase
+      .from('orders')
+      .select(
+        '*, client:clients(*), order_items(created_at, id, order_id, product_id, quantity, product:products(*, brewery:breweries(*)))'
+      )
+      .order('order_date', { ascending: false });
+    return orderlist;
+  };
+
+  const orderlist = await getAllOrder();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="prose">
