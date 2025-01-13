73804bbde4166fa2307a21d9f48739806d48166d
chell2 <rei1259@gmail.com>
2025-01-13 12:49:17 +0900

    フォーム修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,56 @@ import { login, signup } from './actions';
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <div className="card bg-base-100 w-full shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    パスワードを忘れた方
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-2">
+                <button className="btn btn-primary" formAction={login}>
+                  ログイン
+                </button>
+              </div>
+              <div className="form-control mt-2">
+                <button className="btn btn-secondary" formAction={signup}>
+                  新規登録
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
