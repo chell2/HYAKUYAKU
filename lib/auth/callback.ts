55650c04f01df179cda5cb01c95c42a4312df14a
chell2 <rei1259@gmail.com>
2025-01-16 16:49:30 +0900

    不要ファイル削除

--- lib/auth/callback.ts
+++
@@ -1,9 +0,0 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-
-export async function handleAuthCallback(code: string) {
-  if (code) {
-    const supabase = createRouteHandlerClient({ cookies });
-    await supabase.auth.exchangeCodeForSession(code);
-  }
-}
