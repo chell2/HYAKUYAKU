1ccc8f9c88f2ef1b70bd68cb6e29917d3439a1e9
chell2 <rei1259@gmail.com>
2025-01-16 16:00:01 +0900

    セッション管理

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,17 @@
 import AccountForm from './account-form';
 import { createClient } from '@/lib/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default async function Account() {
+export default async function ProfilePage() {
   const supabase = await createClient();
-
   const {
     data: { user },
+    error,
   } = await supabase.auth.getUser();
+
+  if (error || !user) {
+    return redirect('/login');
+  }
 
   return <AccountForm user={user} />;
 }
