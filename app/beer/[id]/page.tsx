29996fa0d3730a7960003a4b6bf59bdf450c7fdc
chell2 <rei1259@gmail.com>
2025-01-17 15:02:52 +0900

    商品情報修正

--- app/beer/[id]/page.tsx
+++ app/beer/[id]/page.tsx
@@ -5,7 +5,6 @@ import {
 import { cookies } from 'next/headers';
 import { Database } from '@/types/supabase';
 import React from 'react';
-import Image from 'next/image';
 
 const getDetailBeer = async (
   id: string,
@@ -19,27 +18,104 @@ const getDetailBeer = async (
   return beer;
 };
 
+const getBreweryData = async (
+  id: string,
+  supabase: SupabaseClient<Database>
+) => {
+  const { data: brewery } = await supabase
+    .from('breweries')
+    .select('*')
+    .eq('id', id)
+    .single();
+  return brewery;
+};
+
 const BeerDetailPage = async ({ params }: { params: { id: string } }) => {
   const supabase = createServerComponentClient<Database>({ cookies });
   const beer = await getDetailBeer(params.id, supabase);
-
+  const formattedPrice = beer?.price
+    ? new Intl.NumberFormat('ja-JP', {
+        style: 'currency',
+        currency: 'JPY',
+      }).format(beer.price)
+    : null;
+  const brewery =
+    beer && beer.brewery_id
+      ? await getBreweryData(beer.brewery_id, supabase)
+      : null;
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="prose">
-        <h1>{beer?.name}</h1>
-      </div>
+    <div className="grid gap-4 justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-        <Image
-          src={`/${beer?.id ?? 'placeholder'}.png`}
-          alt={beer?.name || 'No Name'}
-          width={300}
-          height={100}
-        />
-        <p>{beer?.style}</p>
-        <p>
-          ABV: {beer?.abv} / IBU: {beer?.ibu}
-        </p>
-        <p>{beer?.description}</p>
+        <div className="w-full text-center mb-8 py-4">
+          <div className="indicator">
+            {beer?.status && (
+              <span className="indicator-item badge badge-accent">
+                {beer?.status}
+              </span>
+            )}
+            <div className="stats shadow">
+              <div className="stat">
+                <div className="stat-value text-primary px-10 py-4">
+                  {beer?.name}
+                </div>
+                <div className="stat-title">
+                  {formattedPrice && <p>{formattedPrice}</p>}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid justify-items-center">
+          <img
+            src={`/${beer?.id}.png`}
+            alt=""
+            className="aspect-square w-72 rounded-md object-cover"
+          />
+        </div>
+        <article className="prose mt-8">
+          <p>{beer?.description}</p>
+          <div className="divider divider-start mt-12">【商品情報】</div>
+          <ul className="list-none">
+            <li>商品名： {beer?.name}</li>
+            {beer?.price && <li>価格： {formattedPrice}</li>}
+            <li>
+              ブルワリー：{' '}
+              <a href={`/brewery/${brewery?.id}`} className="link">
+                {brewery?.name}
+              </a>
+            </li>
+            <li>地域： {brewery?.region}</li>
+            {beer?.fermentation !== null && beer?.fermentation !== '-' && (
+              <li>
+                発酵：
+                {beer?.fermentation === 'top' && '上面発酵'}
+                {beer?.fermentation === 'bottom' && '下面発酵'}
+                {beer?.fermentation !== 'top' &&
+                  beer?.fermentation !== 'bottom' &&
+                  beer?.fermentation}
+              </li>
+            )}
+            {beer?.abv !== null && beer?.abv !== '-' && (
+              <li>ABV： {beer?.abv}%</li>
+            )}
+            {beer?.ibu !== null && <li>IBU： {beer?.ibu}</li>}
+            {Array.isArray(beer?.hops) && beer?.hops.length > 0 && (
+              <li>ホップ： {beer?.hops.join(', ')}</li>
+            )}
+            {Array.isArray(beer?.malts) && beer?.malts.length > 0 && (
+              <li>モルト： {beer?.malts.join(', ')}</li>
+            )}
+            {Array.isArray(beer?.others) && beer?.others.length > 0 && (
+              <li>その他： {beer?.others.join(', ')}</li>
+            )}
+            {beer?.volume !== null && (
+              <li>
+                内容量： {beer?.volume}ml {beer?.is_canned === true && '（缶）'}
+                {beer?.is_canned === false && '（瓶）'}
+              </li>
+            )}
+          </ul>
+        </article>
       </main>
     </div>
   );
--- types/supabase.ts
+++ types/supabase.ts
@@ -4,316 +4,331 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       breweries: {
         Row: {
-          created_at: string
-          description: string | null
-          id: string
-          name: string | null
-          region: string | null
-        }
+          created_at: string;
+          description: string | null;
+          id: string;
+          name: string | null;
+          region: string | null;
+        };
         Insert: {
-          created_at?: string
-          description?: string | null
-          id?: string
-          name?: string | null
-          region?: string | null
-        }
+          created_at?: string;
+          description?: string | null;
+          id?: string;
+          name?: string | null;
+          region?: string | null;
+        };
         Update: {
-          created_at?: string
-          description?: string | null
-          id?: string
-          name?: string | null
-          region?: string | null
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          description?: string | null;
+          id?: string;
+          name?: string | null;
+          region?: string | null;
+        };
+        Relationships: [];
+      };
       clients: {
         Row: {
-          created_at: string
-          id: string
-          name: string | null
-          type: string | null
-        }
+          created_at: string;
+          id: string;
+          name: string | null;
+          type: string | null;
+        };
         Insert: {
-          created_at?: string
-          id?: string
-          name?: string | null
-          type?: string | null
-        }
+          created_at?: string;
+          id?: string;
+          name?: string | null;
+          type?: string | null;
+        };
         Update: {
-          created_at?: string
-          id?: string
-          name?: string | null
-          type?: string | null
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          id?: string;
+          name?: string | null;
+          type?: string | null;
+        };
+        Relationships: [];
+      };
       order_items: {
         Row: {
-          created_at: string
-          id: string
-          order_id: string | null
-          product_id: string | null
-          quantity: number | null
-        }
+          created_at: string;
+          id: string;
+          order_id: string | null;
+          product_id: string | null;
+          quantity: number | null;
+        };
         Insert: {
-          created_at?: string
-          id?: string
-          order_id?: string | null
-          product_id?: string | null
-          quantity?: number | null
-        }
+          created_at?: string;
+          id?: string;
+          order_id?: string | null;
+          product_id?: string | null;
+          quantity?: number | null;
+        };
         Update: {
-          created_at?: string
-          id?: string
-          order_id?: string | null
-          product_id?: string | null
-          quantity?: number | null
-        }
+          created_at?: string;
+          id?: string;
+          order_id?: string | null;
+          product_id?: string | null;
+          quantity?: number | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "delivery_items_delivery_id_fkey"
-            columns: ["order_id"]
-            isOneToOne: false
-            referencedRelation: "orders"
-            referencedColumns: ["id"]
+            foreignKeyName: 'delivery_items_delivery_id_fkey';
+            columns: ['order_id'];
+            isOneToOne: false;
+            referencedRelation: 'orders';
+            referencedColumns: ['id'];
           },
           {
-            foreignKeyName: "delivery_items_product_id_fkey"
-            columns: ["product_id"]
-            isOneToOne: false
-            referencedRelation: "products"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'delivery_items_product_id_fkey';
+            columns: ['product_id'];
+            isOneToOne: false;
+            referencedRelation: 'products';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       orders: {
         Row: {
-          client_id: string | null
-          created_at: string
-          id: string
-          order_date: string | null
-        }
+          client_id: string | null;
+          created_at: string;
+          id: string;
+          order_date: string | null;
+        };
         Insert: {
-          client_id?: string | null
-          created_at?: string
-          id?: string
-          order_date?: string | null
-        }
+          client_id?: string | null;
+          created_at?: string;
+          id?: string;
+          order_date?: string | null;
+        };
         Update: {
-          client_id?: string | null
-          created_at?: string
-          id?: string
-          order_date?: string | null
-        }
+          client_id?: string | null;
+          created_at?: string;
+          id?: string;
+          order_date?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "deliveries_client_id_fkey"
-            columns: ["client_id"]
-            isOneToOne: false
-            referencedRelation: "clients"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'deliveries_client_id_fkey';
+            columns: ['client_id'];
+            isOneToOne: false;
+            referencedRelation: 'clients';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       products: {
         Row: {
-          abv: string | null
-          brewery_id: string | null
-          created_at: string
-          description: string | null
-          fermentation: string | null
-          hops: string[] | null
-          ibu: number | null
-          id: string
-          malts: string[] | null
-          name: string | null
-          style: string | null
-          volume: number | null
-        }
+          abv: string | null;
+          brewery_id: string | null;
+          created_at: string;
+          description: string | null;
+          fermentation: string | null;
+          hops: string[] | null;
+          ibu: number | null;
+          id: string;
+          is_canned: boolean;
+          malts: string[] | null;
+          name: string | null;
+          others: string[] | null;
+          price: number | null;
+          status: string | null;
+          style: string | null;
+          volume: number | null;
+          yeast: string[] | null;
+        };
         Insert: {
-          abv?: string | null
-          brewery_id?: string | null
-          created_at?: string
-          description?: string | null
-          fermentation?: string | null
-          hops?: string[] | null
-          ibu?: number | null
-          id?: string
-          malts?: string[] | null
-          name?: string | null
-          style?: string | null
-          volume?: number | null
-        }
+          abv?: string | null;
+          brewery_id?: string | null;
+          created_at?: string;
+          description?: string | null;
+          fermentation?: string | null;
+          hops?: string[] | null;
+          ibu?: number | null;
+          id?: string;
+          is_canned?: boolean;
+          malts?: string[] | null;
+          name?: string | null;
+          others?: string[] | null;
+          price?: number | null;
+          status?: string | null;
+          style?: string | null;
+          volume?: number | null;
+          yeast?: string[] | null;
+        };
         Update: {
-          abv?: string | null
-          brewery_id?: string | null
-          created_at?: string
-          description?: string | null
-          fermentation?: string | null
-          hops?: string[] | null
-          ibu?: number | null
-          id?: string
-          malts?: string[] | null
-          name?: string | null
-          style?: string | null
-          volume?: number | null
-        }
+          abv?: string | null;
+          brewery_id?: string | null;
+          created_at?: string;
+          description?: string | null;
+          fermentation?: string | null;
+          hops?: string[] | null;
+          ibu?: number | null;
+          id?: string;
+          is_canned?: boolean;
+          malts?: string[] | null;
+          name?: string | null;
+          others?: string[] | null;
+          price?: number | null;
+          status?: string | null;
+          style?: string | null;
+          volume?: number | null;
+          yeast?: string[] | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "products_brewery_id_fkey"
-            columns: ["brewery_id"]
-            isOneToOne: false
-            referencedRelation: "breweries"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'products_brewery_id_fkey';
+            columns: ['brewery_id'];
+            isOneToOne: false;
+            referencedRelation: 'breweries';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       profile: {
         Row: {
-          created_at: string
-          id: string
-          is_admin: boolean
-          name: string | null
-          role: string | null
-          updated_at: string
-          user_number: number
-        }
+          created_at: string;
+          id: string;
+          is_admin: boolean;
+          name: string | null;
+          role: string | null;
+          updated_at: string;
+          user_number: number;
+        };
         Insert: {
-          created_at?: string
-          id?: string
-          is_admin?: boolean
-          name?: string | null
-          role?: string | null
-          updated_at?: string
-          user_number?: number
-        }
+          created_at?: string;
+          id?: string;
+          is_admin?: boolean;
+          name?: string | null;
+          role?: string | null;
+          updated_at?: string;
+          user_number?: number;
+        };
         Update: {
-          created_at?: string
-          id?: string
-          is_admin?: boolean
-          name?: string | null
-          role?: string | null
-          updated_at?: string
-          user_number?: number
-        }
-        Relationships: []
-      }
-    }
+          created_at?: string;
+          id?: string;
+          is_admin?: boolean;
+          name?: string | null;
+          role?: string | null;
+          updated_at?: string;
+          user_number?: number;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
 
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] &
+      PublicSchema['Views'])
+  ? (PublicSchema['Tables'] &
+      PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+    | keyof PublicSchema['Tables']
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+    | keyof PublicSchema['Tables']
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+    | keyof PublicSchema['Enums']
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+  ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+  : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof PublicSchema["CompositeTypes"]
+    | keyof PublicSchema['CompositeTypes']
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+    ? keyof Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes']
+    : never = never
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
-  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
-    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? Database[PublicCompositeTypeNameOrOptions['schema']]['CompositeTypes'][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema['CompositeTypes']
+  ? PublicSchema['CompositeTypes'][PublicCompositeTypeNameOrOptions]
+  : never;
