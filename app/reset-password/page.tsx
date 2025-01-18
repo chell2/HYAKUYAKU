b088bfb8b1acd63355cb8edb015cf1b22ce02c01
chell2 <rei1259@gmail.com>
2025-01-18 10:48:14 +0900

    フォーム切り出し

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,7 +1,7 @@
 import { Message } from '@/components/FormMessage';
 import ResetPasswordForm from './reset-password-form';
 
-export default async function ResetPassword() {
+export default async function ResetPasswordPage() {
   return (
     <div className="bg-base-200 flex justify-center items-center min-h-screen">
       <div className="max-w-md w-full">
